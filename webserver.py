720173484d7b7eb6e2fb893416061c7a693d41fa
Ross Voerman <voermanr@oregonstate.edu>
2023-10-12 13:05:58 -0700

    first pass at parsing the file name

--- webserver.py
+++ webserver.py
@@ -1,6 +1,15 @@
+import os.path
 import socket
 import sys
 import re
+
+
+def parse_file_name(r: str):
+    split_request = r.split("\r\n")
+    split_request = split_request[0].split(' ')
+    file_path = os.path.split(split_request[1])
+    return file_path[1]
+
 
 port = 28333
 response = b'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 6\r\nConnection: close\r\n\r\nHello!\r\n'
@@ -17,9 +26,16 @@ while True:
     connected_sock = incoming[0]
     print(str(incoming[1][0]) + ":" + str(incoming[1][1]) + " connected...")
 
-    request = connected_sock.recv(4096).decode()
-    while not re.search(r'\r\n\r\n', request):
+    request_buff = ''
+    while True:
         request = connected_sock.recv(4096).decode()
+        request_buff += request
+        if re.search(r'\r\n\r\n', request):
+            break
+
+    file_name = parse_file_name(request_buff)
+
+    print("file name: " + file_name)
 
     connected_sock.sendall(response)
     connected_sock.close()
